68bbbc1f08beab3137da275c6ab79d71b672567b
repalleramr <repalleramr@gmail.com>
2026-05-13 18:52:45 +0530

    Fetch data from Cricbuzz API and return response

--- api/live.js
+++ api/live.js
@@ -1,8 +1,23 @@
+const axios = require('axios');
+
 module.exports = async function (req, res) {
 
-  return res.status(200).json({
-    success: true,
-    msg: "LIVE API WORKING"
-  });
+  try {
 
+    const r = await axios.get(
+      'https://www.cricbuzz.com'
+    );
+
+    return res.status(200).json({
+      success: true,
+      length: r.data.length
+    });
+
+  } catch (e) {
+
+    return res.status(200).json({
+      success: false,
+      error: e.message
+    });
+  }
 };
